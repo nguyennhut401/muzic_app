5a1f9c39c5b4fd4cb1dc40571f5c5b91138eb6cc
nhut nguyen <nhut.nguyen@edge-works.net>
2019-07-26 15:27:30 +0700

    get previous instance for storing changes

--- socket_server/orm/models/common.py
+++ socket_server/orm/models/common.py
@@ -6,9 +6,10 @@ from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 from django.core import serializers
 from django.db import models
-from django.db.models.signals import (post_delete, post_save)
+from django.db.models.signals import (post_delete, post_save, pre_save)
 from django.dispatch import receiver
 from django.conf import settings
+
 
 
 class EnumChoice(enum.Enum):
@@ -17,15 +18,24 @@ class EnumChoice(enum.Enum):
         return ((obj.name, obj.value) for obj in cls)
 
 class CommonModel(models.Model):
+    previous_inst = None
+
     created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     def __init__(self, *args, **kwargs):
         post_save.connect(self.save_signal, sender=self.__class__)
-
         post_delete.connect(self.delete_signal, sender=self.__class__)
 
+        pre_save.connect(self.pre_save_signal, sender=self.__class__)
+
         return super().__init__(*args, **kwargs)
+
+
+    def pre_save_signal(self, sender, **kwargs):
+        instance = kwargs['instance']
+
+        self.previous_inst = instance
 
     
     def save_signal(self, sender, **kwargs):
@@ -43,13 +53,11 @@ class CommonModel(models.Model):
         })
 
     def build_signal_object(self, instance, action):
-        import pdb; pdb.set_trace()
-
         package_obj = {
             'table': instance.__class__.__name__,
             'action': action,
             'detail': {
-                'from': {},
+                'from': serializers.serialize('json', [self.previous_inst]),
                 'to': serializers.serialize('json', [instance])
             }
         }
